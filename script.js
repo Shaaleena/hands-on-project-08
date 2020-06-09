054306e68eb63743121e5fe5752874055faa4d63
Alina Sharafutdinova <49404655+Shaaleena@users.noreply.github.com>
2020-06-09 13:25:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,26 @@
 "use strict"; //setting document contents in strict mode
 var list=[];
 
-function generateList(){
+function generateList(){ //function that creating the list of items
   var listItems = document.getElementsByTagName("li");
   for (var i=listItems.length-1;i>=0;i--){
     document.getElementsByTagName("ol")[0].removeChild(listItems[i]);
   }
   for(var i=0;i<list.length;i++){ //loops through existing li elements and removes them all
-    var newItem="<span class='first'>first</span>"+list[i];
+    var newItem="<span class='first'>first</span>"+"<span class='last'>last</span>"+list[i];
     var newListItem = document.createElement("li"); //creates new li element for each array element
     newListItem.innerHTML=newItem;
     document.getElementsByTagName("ol")[0].appendChild(newListItem);
     var firstButtons=document.querySelectorAll(".first");
     var lastFirstButton=firstButtons[firstButtons.length-1];
-    if(lastFirstButton.addEventListener){ //adds event listenere to the "first" button
+    var lastButtons=document.querySelectorAll(".last");
+    var lastLastButton=lastButtons[lastButtons.length-1];
+    if(lastFirstButton.addEventListener){ //adds event listenere to the "first" and "last" buttons
       lastFirstButton.addEventListener("click", moveToTop,false);
+      lastLastButton.addEventListener("click", moveToBottom, false);
   } else if(lastFirstButton.attachEvent){
     lastFirstButton.attachEvent("onclick", moveToTop);
+    lastLastButton.attachEvent("onclick", moveToBottom);
   }
 }
 }
@@ -31,7 +35,7 @@ function addItem(){ //adds the value to the end of the list array using push met
   generateList();
 }
 
-function moveToTop(evt) { //creates
+function moveToTop(evt) { //function that moves item on the top when First button clicked
   if(evt===undefined){
     evt=window.event;
   }
@@ -47,7 +51,22 @@ function moveToTop(evt) { //creates
   generateList();
 }
 
-function createEventListener(){
+function moveToBottom(evt){ //function that moves item at the bottom when Last button clicked
+  if(evt === undefined){
+      evt=window.event;
+  } var callerElement=evt.target||evt.srcElement;
+  var listItems=document.getElementsByTagName("li");
+  var parentItem=callerElement.parentNode;
+  for(var i=0;i<list.length;i++){
+    if(parentItem.innerHTML.search(list[i])!== -1){
+      var itemToMove=list.splice(i,1);
+      list.push(itemToMove);
+    }
+  }
+  generateList();
+}
+
+function createEventListener(){ //event listener for adding item button
   var addButton=document.getElementById("button");
   if(addButton.addEventListener){
     addButton.addEventListener("click", addItem, false);
